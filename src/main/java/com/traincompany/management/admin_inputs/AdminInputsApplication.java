1f08d02abbcb75912cbb6ad5b86f788147c3fc52
Gabriel Liberato <gabrielliberato@Gabriels-MacBook-Pro.local>
2024-08-15 22:27:21 -0400

    Fist commit - Adding datasource

---
+++ src/main/java/com/traincompany/management/admin_inputs/AdminInputsApplication.java
@@ -0,0 +1,13 @@
+package com.traincompany.management.admin_inputs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminInputsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminInputsApplication.class, args);
+	}
+
+}
